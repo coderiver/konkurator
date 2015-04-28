e12378bb2774d310d8de9dde962805e28a2f370b
Валерий <kaktuz34@gmail.com>
2015-04-28 10:41:47 +0300

    home page

--- js/common.js
+++ js/common.js
@@ -1,19 +1,80 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// slider
+	$('.js-slider').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		autoplay: true,
+		cssEase: 'linear',
+		autoplaySpeed: 3000,
+		pauseOnHover: true,
+		slidesToShow: 1
+	});
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	// tab
+	function tab() {
+		$(".js-tab").each(function(){
+			var tab_link = $(this).find("a");
+			var tab_item = $(this).find("li");
+			var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+			$(this).parents(".js-tab-group").find(".js-tab-cont:first").addClass('is-active');
+			$(this).find("li:first").addClass('is-active');
+			tab_link.on("click", function() {
+				var index = $(this).attr("href");
+				$('.js-tab-item').removeClass("is-active");
+				$(this).parent().addClass("is-active");
+				tab_cont.removeClass('is-active');
+				$(this).parents(".js-tab-group").find("."+index).addClass("is-active");
+				return false;
+			});
+		});
+	}
+	tab();
 
-	console.log($('body').html());
+	// maps
+	var map;
+	var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+
+	function ZoomControl(controlDiv, map) {
+
+		var controlWrapper = document.createElement('div');
+		controlWrapper.className = 'js-zoom';
+		controlDiv.appendChild(controlWrapper);
+
+		var zoomInButton = document.createElement('div');
+		controlWrapper.appendChild(zoomInButton);
+
+		google.maps.event.addDomListener(zoomInButton, 'click', function() {
+		  map.setZoom(map.getZoom() + 1);
+		}); 
+
+	}
+
+	$('.js-zoom-map').on('click', function () {
+		$('.js-zoom > div').trigger('click');
+	});
+
+	function initialize() {
+		var mapDiv = document.getElementById('map');
+
+		var mapOptions = {
+		  zoom: 12,
+		  center: chicago,
+		  disableDefaultUI: true
+		}
+
+		map = new google.maps.Map(mapDiv, mapOptions);
+
+		var zoomControlDiv = document.createElement('div');
+		var zoomControl = new ZoomControl(zoomControlDiv, map);
+
+		zoomControlDiv.index = 200;
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(zoomControlDiv);
+	}
+
+	initialize();
+
 });
